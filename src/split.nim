7068e190e3060e0ed43db35fc75008382674b8fe
Krystian Safjan <ksafjan@gmail.com>
2020-04-22 15:47:31 +0200

    Clean up commented code

--- src/split.nim
+++ src/split.nim
@@ -82,20 +82,3 @@ proc gini_index*(groups: (DataSet, DataSet), classes: seq[char]): float =
 #     for f_idx in feature_idx:
 #         for row in feature_matrix:
 #             var groups = test_split(index, row[index], dataset)
-
-
-# def get_split(dataset, n_features):
-# class_values = list(set(row[-1] for row in dataset))
-# b_index, b_value, b_score, b_groups = 999, 999, 999, None
-# features = list()
-# while len(features) < n_features:
-# 	index = randrange(len(dataset[0])-1)
-# 	if index not in features:
-# 		features.append(index)
-# for index in features:
-# 	for row in dataset:
-# 		groups = test_split(index, row[index], dataset)
-# 		gini = gini_index(groups, class_values)
-# 		if gini < b_score:
-# 			b_index, b_value, b_score, b_groups = index, row[index], gini, groups
-# return {'index':b_index, 'value':b_value, 'groups':b_groups}
