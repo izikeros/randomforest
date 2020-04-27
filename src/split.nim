f42d66b98e3a00159b485a9c41becfb41edcc131
Krystian Safjan <ksafjan@gmail.com>
2020-04-27 07:30:02 +0200

    Add initial implementation for split and tests

--- src/split.nim
+++ src/split.nim
@@ -1,6 +1,69 @@
 import random
 import sequtils
 import data_types
+import tables
+import utils
+
+const
+    MAX_IDX = 999
+    MAX_VAL = 999.0
+    MAX_SCORE = 999.0
+
+type
+    Split* = ref object of RootObj
+        feature_idx*: int
+        value*: float
+        groups*: (DataSet, DataSet)
+        left*: DataSet
+        right*: DataSet
+
+proc to_terminal*(group: DataSet): Label = 
+    ## Create a terminal node value
+    let labels = group[1]
+    
+    # count labels of the same type
+    var labelFrequencies = labels.toCountTable
+    # sort 
+    labelFrequencies.sort
+    # take most frequent
+    let pair = toSeq(labelFrequencies.pairs)[0]
+    # return label name as result
+    result = pair[0]
+
+# Create child splits for a node or make terminal
+proc split*(node: Split, max_depth: int, min_size: int, n_features: int, depth: int) =
+    let 
+        left = node.groups[0]
+        right = node.groups[1]
+    echo left
+    echo right
+
+    # node.groups = (@[], @[]) # TODO: KS: 2020-04-23: clear groups
+    # check for a no split (the other group is empty)
+    if (left[1].len == 0) or (right[1].len == 0):
+        let f: DataSet = merge_folds(@[left, left])
+        node.left = to_terminal(f) # FIXME: KS: 2020-04-24: Need to solve type clarity
+        #node.right = node.left
+        return
+    # # check for max depth
+    # if depth >= max_depth:
+    #     node.left = to_terminal(left)
+    #     node.right = to_terminal(right)
+    #     return
+    # # process left child
+    # if len(left) <= min_size:
+    #     node['left'] = to_terminal(left)
+    # else:
+    #     node['left'] = get_split(left, n_features)
+    #     split(node['left'], max_depth, min_size, n_features, depth+1)
+#     # process right child
+#     if len(right) <= min_size:
+#         node['right'] = to_terminal(right)
+#     else:
+#         node['right'] = get_split(right, n_features)
+#         split(node['right'], max_depth, min_size, n_features, depth+1)
+
+
 
 # Split samples based on given feature and threshold value
 proc test_split*(index: int, value: float, dataset: DataSet): (DataSet, DataSet) =
@@ -8,7 +71,7 @@ proc test_split*(index: int, value: float, dataset: DataSet): (DataSet, DataSet)
         left: DataSet
         right: DataSet
 
-    # TODO: split to separate features and labels variables can be replaced with some unzip/zip?
+    # TODO: KS: 2020-04-23:  split to separate features and labels variables can be replaced with some unzip/zip?
     let features = dataset[0]
     let labels = dataset[1]
 
@@ -36,7 +99,7 @@ proc group_weighted_gini*(group: DataSet, classes: seq[char], n_samples: int): f
 
     if size > 0:
         # score the group based on the score for each class
-        # TODO: consider using CounterSet from tables
+        # TODO: KS: 2020-04-22:  consider using CounterSet from tables
         for class_val in classes:
             p = float(count(group[1], class_val)) / size.float
             score += p * p
@@ -51,34 +114,41 @@ proc gini_index*(groups: (DataSet, DataSet), classes: seq[char]): float =
     var n_samples: int
     n_samples = groups[0][1].len
     n_samples += groups[1][1].len
-    var cl: seq[char] = @['A','B']  # FIXME
     # sum weighted Gini index for each group
-    result = group_weighted_gini(groups[0], classes=cl, n_samples)
-    result += group_weighted_gini(groups[1], classes=cl, n_samples)
+    result = group_weighted_gini(groups[0], classes=classes, n_samples)
+    result += group_weighted_gini(groups[1], classes=classes, n_samples)
 
-# proc get_split(dataset: seq[seq[float]], n_rf_features: int, labels: seq[string]): tuple =
-#     # make list of feature indices that we will make split on
-    
-#     const MAX_IDX = 999
-#     const MAX_VAL = 999
-#     const MAX_SCORE = 999
 
-#     var 
-#         b_index = 999
-#         b_value = 999
-#         b_score = 999
-#         b_groups = @[]
-#         i_feature: int
-    
-#     let n_features = 100
+proc get_split*(dataset: DataSet, n_rf_features: int): Split =
+    # make list of feature indices that we will make split on
 
-#     let feature_matrix = dataset[0]
+    var 
+        b_index = MAX_IDX
+        b_value = MAX_VAL
+        b_score = MAX_SCORE
+        b_groups: (DataSet, DataSet)
+        i_feature: int
+        gini: float
+
+    let 
+        feature_matrix = dataset[0]
+        n_features = feature_matrix[0].len
+        classes = deduplicate(dataset[1])
+        # shuffle order of features
+        feature_idx = toSeq(0 .. n_features-1)
     
-#     # shuffle order of features
-#     let feature_idx = toSeq(1 .. n_features)
-    
-#     # limit number of features taken as split points
-#     feature_idx = feature_idx[1:n_rf_features]
-#     for f_idx in feature_idx:
-#         for row in feature_matrix:
-#             var groups = test_split(index, row[index], dataset)
+    # limit number of features taken as split points
+    let feature_lim_idx = feature_idx[0 .. n_rf_features-1]
+    for f_idx in feature_lim_idx:
+        for row in feature_matrix:
+            var groups = test_split(f_idx, row[f_idx], dataset)
+            gini = gini_index(groups, classes)
+            if gini < b_score:
+                b_index = f_idx
+                b_value = row[f_idx]
+                b_score = gini
+                b_groups = groups
+    result = Split(
+        feature_idx: b_index,
+        value: b_value,
+        groups: b_groups)
--- tests/split_test.nim
+++ tests/split_test.nim
@@ -4,53 +4,73 @@ import ../src/split
 
 let
     r1: DataRow = @[3.0, 2.1, 10.0, 3.0, 7.0]
-    r2: DataRow = @[6.0, 4.0, 5.1,  8.0, 5.0]
+    r2: DataRow = @[6.0, 4.0, 5.1, 8.0, 5.0]
+    r3: DataRow = @[13.0, 12.1, 10.0, 3.0, 7.0]
+    r4: DataRow = @[16.0, 14.0, 5.1, 8.0, 5.0]
+
     l1: char = 'A'
     l2: char = 'B'
-    dataset = (@[r1, r2], @[l1,l2])
+    dataset = (@[r1, r2], @[l1, l2])
+    dataset4 = (@[r1, r2, r3, r4], @[l1, l2, l2, l2])
     empty: (seq[DataRow], seq[char]) = (@[], @[])
 
-suite "Group gini":
-    test "total: two samples, group: two samples, two classes":
-        let g = group_weighted_gini(dataset, @[l1,l2], n_samples=2)
-        check g == 0.5
+suite "Split":
+    test "base":
+        let s = Split(groups: (dataset,dataset4))
+        split(node=s, max_depth=10, min_size=0, n_features=10, depth=10)
 
-    test "total: four samples, group: two samples, two classes":
-        let g = group_weighted_gini(dataset, @[l1,l2], n_samples=4)
-        check g == 0.25
+# suite "To terminal":
+#     test "base":        
+#         check to_terminal(dataset4) == 'B'
 
-suite "Test split":
-    test "one above, one below: feature_id:0 val:4.0":
-        let res = test_split(index=0, value=4.0, dataset=dataset)
-        let lt = res[0]
-        let ge = res[1]
-        check lt == (@[r1], @[l1])
-        check ge == (@[r2], @[l2])
+# suite "Group gini":
+#     test "total: two samples, group: two samples, two classes":
+#         let g = group_weighted_gini(dataset, @[l1,l2], n_samples=2)
+#         check g == 0.5
 
-    test "one equal, one below: feature_id:0 val:6.0":
-        let res = test_split(index=0, value=6.0, dataset=dataset)
-        let lt = res[0]
-        let ge = res[1]
-        check lt == (@[r1], @[l1])
-        check ge == (@[r2], @[l2])
-        
-    test "one above, one below: feature_id:2 val:9.0":
-        let res = test_split(index=2, value=9.0, dataset=dataset)
-        let lt = res[0]
-        let ge = res[1]
-        check lt == (@[r2], @[l2])
-        check ge == (@[r1], @[l1])
+#     test "total: four samples, group: two samples, two classes":
+#         let g = group_weighted_gini(dataset, @[l1,l2], n_samples=4)
+#         check g == 0.25
 
-    test "none lower: feature_id:0 val:3.0":
-        let res = test_split(index=0, value=3.0, dataset=dataset)
-        let lt = res[0]
-        let ge = res[1]
-        check lt == empty
-        check ge == (@[r1,r2], @[l1,l2])
+# suite "Test split":
+#     test "one above, one below: feature_id:0 val:4.0":
+#         let res = test_split(index=0, value=4.0, dataset=dataset)
+#         let lt = res[0]
+#         let ge = res[1]
+#         check lt == (@[r1], @[l1])
+#         check ge == (@[r2], @[l2])
 
-    test "both lower: feature_id:1 val:5.0":
-        let res = test_split(index=1, value=5.0, dataset=dataset)
-        let lt = res[0]
-        let ge = res[1]
-        check lt == (@[r1,r2], @[l1,l2])
-        check ge == empty
+#     test "one equal, one below: feature_id:0 val:6.0":
+#         let res = test_split(index=0, value=6.0, dataset=dataset)
+#         let lt = res[0]
+#         let ge = res[1]
+#         check lt == (@[r1], @[l1])
+#         check ge == (@[r2], @[l2])
+
+#     test "one above, one below: feature_id:2 val:9.0":
+#         let res = test_split(index=2, value=9.0, dataset=dataset)
+#         let lt = res[0]
+#         let ge = res[1]
+#         check lt == (@[r2], @[l2])
+#         check ge == (@[r1], @[l1])
+
+#     test "none lower: feature_id:0 val:3.0":
+#         let res = test_split(index=0, value=3.0, dataset=dataset)
+#         let lt = res[0]
+#         let ge = res[1]
+#         check lt == empty
+#         check ge == (@[r1,r2], @[l1,l2])
+
+#     test "both lower: feature_id:1 val:5.0":
+#         let res = test_split(index=1, value=5.0, dataset=dataset)
+#         let lt = res[0]
+#         let ge = res[1]
+#         check lt == (@[r1,r2], @[l1,l2])
+#         check ge == empty
+
+# suite "Get split":
+#     test "is running":
+#         let a: Split = get_split(dataset4, n_rf_features=5)
+#         # echo a.groups[0]
+#         # echo a.groups[1]
+#         # TODO: KS: 2020-04-23: check correctnes
